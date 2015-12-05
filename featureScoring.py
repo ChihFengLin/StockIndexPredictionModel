522424cb0980683e6775216403537d1c02bd19de
Chih-Feng Lin <phone751230@gmail.com>
2015-12-05 13:50:30 -0500

    finish feature scoring function

--- featureScoring.py
+++ featureScoring.py
@@ -3,7 +3,6 @@ import sys
 import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.ensemble import ExtraTreesClassifier
-from sklearn.datasets import make_classification
 
 def parseRawData(fileName):
 
@@ -45,34 +44,51 @@ def parseRawData(fileName):
 	return date_training, date_testing, training, testing, target
 
 
-if __name__ == '__main__':
-	date_training, date_testing, training, testing, target  = parseRawData('stock_returns_base150.csv')
-	print target
-
-#training = genfromtxt('stock_returns_base150.csv', delimiter=',')
-
-#print training
-
-#labels = genfromtxt('labels.csv', delimiter=',')
-#labels = labels.transpose()
-#X = training
-
-weights = np.zeros((labels.shape[0], X.shape[1]))
-
-for classifier_no in range(labels.shape[0]):
-	
-	y = labels[classifier_no][0:]
+def scoreFeature(X, y):
+	weights = np.zeros((1, X.shape[1]))
 
 	# Build a forest and compute the feature importances
 	forest = ExtraTreesClassifier(n_estimators=250, random_state=0)
 
 	forest.fit(X, y)
 	importances = forest.feature_importances_
-	indices = np.argsort(importances)
+	indices = np.argsort(importances)[::-1]
+	ranking = []
 
+	print("Feature Ranking:")
 	for f in range(X.shape[1]):
-		weight = f / float(X.shape[1] - 1)
-		weights[classifier_no][indices[f]-1] = weight
-    	#print("%d. feature %d (%f)" % (f + 1, indices[f], importances[indices[f]]))
+		ranking.append(indices[f])
+		print("%d. feature S%d (%f)" % (f + 1, indices[f] + 2, importances[indices[f]]))
+	return ranking
 
-#np.savetxt('weights.csv', weights, delimiter=',')
+def generateNewFeature(data, ranking):
+	new_dataset = []
+	for row in data:
+		temp = []
+		for rank in ranking[0:5]:      # Use top 5 important feature
+			temp.append(row[rank])
+		row.append(np.mean(temp))   # Calculate their mean
+		row.append(np.std(temp))    # Calculate their std
+		new_dataset.append(row)
+	
+	return new_dataset
+
+
+
+if __name__ == '__main__':
+
+	date_training, date_testing, training, testing, target = parseRawData(sys.argv[1])
+	ranking = scoreFeature(np.matrix(training), target)
+	new_training = generateNewFeature(training, ranking)
+	new_testing = generateNewFeature(testing, ranking)
+	
+	# Save parsed data
+	np.savetxt('training.csv', training, delimiter=',')
+	np.savetxt('testing.csv', testing, delimiter=',')
+	np.savetxt('target.csv', target, delimiter=',')
+	np.savetxt('date_training.csv', date_training, delimiter=',', fmt="%s")
+	np.savetxt('date_testing.csv', date_testing, delimiter=',', fmt="%s")
+
+
+
+
